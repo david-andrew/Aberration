6c1839b686180143f0b1ce50c9e9fa4ead6354d9
David Samson <david.andrew.engineer@gmail.com>
2024-03-12 00:45:32 -0400

    fixed translate towards to actually translate towards the correct target

--- scenes/ThrustModule.gd
+++ scenes/ThrustModule.gd
@@ -4,7 +4,7 @@ class_name ThrustModule
 
 @export var TRANSLATION_STRENGTH:float = 500
 @export var ROTATION_STRENGTH:float = 2
-@export var THRUST_STRENGTH:float = 2500
+@export var THRUST_STRENGTH:float = 1000
 
 var original_parent:RigidBody3D
 
@@ -20,9 +20,9 @@ func point_towards(target:Vector3, strength:float=1.0):
 func translate_towards(target:Vector3, strength:float=1.0):
 	if get_parent() != original_parent:
 		return
-	var local_direction = to_local(target).normalized()
-	original_parent.apply_central_force(local_direction * TRANSLATION_STRENGTH * strength)
-
+	var relative_diff = target - original_parent.global_position
+	original_parent.apply_central_force(relative_diff.normalized() * TRANSLATION_STRENGTH * strength)
+	
 func thrust_forward(strength:float=1.0):
 	if get_parent() != original_parent:
 		return
